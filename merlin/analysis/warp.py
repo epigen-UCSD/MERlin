ae4e47f1fcc752499ddb321f619d7f66d3b26f94
Colin Kern <jckern@ucsd.edu>
2021-11-12 10:04:35 -0800

    Added FiducialBeadWarp task for more robust registration method

--- merlin/analysis/warp.py
+++ merlin/analysis/warp.py
@@ -4,6 +4,7 @@ import numpy as np
 from skimage import transform
 from skimage import registration
 import cv2
+from sklearn.neighbors import NearestNeighbors
 
 from merlin.core import analysistask
 from merlin.util import aberration
@@ -201,6 +202,186 @@ class FiducialCorrelationWarp(Warp):
             self._filter(self.dataSet.get_fiducial_image(x, fragmentIndex)),
             upsample_factor=100)[0] for x in
                    self.dataSet.get_data_organization().get_data_channels()]
+        transformations = [transform.SimilarityTransform(
+            translation=[-x[1], -x[0]]) for x in offsets]
+        self._process_transformations(transformations, fragmentIndex)
+
+class FiducialBeadWarp(Warp):
+    """
+    An analysis task that warps a set of images taken in different imaging
+    rounds based on alignment of local maxima (beads).
+    """
+
+    def __init__(self, dataSet, parameters=None, analysisName=None):
+        super().__init__(dataSet, parameters, analysisName)
+
+        if 'delta' not in self.parameters:
+            self.parameters['delta'] = 2
+        if 'delta_fit' not in self.parameters:
+            self.parameters['delta_fit'] = 3
+        if 'dbscan' not in self.parameters:
+            self.parameters['dbscan'] = True
+        if 'max_disp' not in self.parameters:
+            self.parameters['max_disp'] = 200
+        if 'tile_size' not in self.parameters:
+            self.parameters['tile_size'] = 512
+        if 'filter_size' not in self.parameters:
+            self.parameters['filter_size'] = 15
+        if 'threshold_sigma' not in self.parameters:
+            self.parameters['threshold_sigma'] = 4
+
+    def fragment_count(self):
+        return len(self.dataSet.get_fovs())
+
+    def get_estimated_memory(self):
+        return 2048
+
+    def get_estimated_time(self):
+        return 5
+
+    def get_dependencies(self):
+        return []
+
+    def _filter(self, inputImage: np.ndarray) -> np.ndarray:
+        im = inputImage.astype(np.float32)
+        s = self.parameters['filter_size']
+        return im / cv2.blur(im, (s, s))
+
+    def _get_tiles(self, im):
+        size = self.parameters['tile_size']
+        sx, sy = im.shape
+        Mx = int(np.ceil(sx / float(size)))
+        My = int(np.ceil(sy / float(size)))
+        ims_dic = {}
+        for ix in range(Mx):
+            for iy in range(My):
+                ims_dic[(ix, iy)] = ims_dic.get((ix, iy), [])+[im[ix * size:(ix + 1) * size, iy * size:(iy + 1) * size]]
+        return ims_dic
+
+    def _get_local_max(self, im_dif, th_fit):
+        """Given a 2D image <im_dif> as numpy array, get the local maxima in square -<delta>_to_<delta>.
+        Optional a dbscan can be used to couple connected pixels with the same local maximum.
+        (This is important if saturating the camera values.)
+        Returns: Xh - a list of x,y and brightness of the local maxima
+        """
+        delta = self.parameters['delta']
+        delta_fit = self.parameters['delta_fit']
+        x, y = np.where(im_dif > th_fit)
+        xmax, ymax = im_dif.shape
+        in_im = im_dif[x, y]
+        keep = np.ones(len(x)) > 0
+        for d2 in range(-delta, delta+1):
+            for d3 in range(-delta, delta+1):
+                keep &= (in_im >= im_dif[(x + d2) % xmax, (y + d3) % ymax])
+        x, y = x[keep], y[keep]
+        h = in_im[keep]
+        Xh = np.array([x, y, h]).T
+        if self.parameters['dbscan'] and len(Xh) > 0:
+            from scipy import ndimage
+            im_keep = np.zeros(im_dif.shape, dtype=bool)
+            im_keep[x, y] = True
+            lbl, nlbl = ndimage.label(im_keep, structure=np.ones([3]*2))
+            l = lbl[x, y]  # labels after reconnection
+            ul = np.arange(1, nlbl + 1)
+            il = np.argsort(l)
+            l = l[il]
+            x, y, h = x[il], y[il], h[il]
+            inds = np.searchsorted(l, ul)
+            Xh = np.array([x, y, h]).T
+            Xh_ = []
+            for i_ in range(len(inds)):
+                j_ = inds[i_ + 1] if i_ < len(inds) - 1 else len(Xh)
+                Xh_.append(np.mean(Xh[inds[i_]:j_], 0))
+            Xh = np.array(Xh_)
+            x, y, h = Xh.T
+        im_centers = []
+        if delta_fit != 0 and len(Xh) > 0:
+            x, y, h = Xh.T
+            x, y = x.astype(int), y.astype(int)
+            im_centers = [[], [], []]
+            for d2 in range(-delta_fit, delta_fit + 1):
+                for d3 in range(-delta_fit, delta_fit + 1):
+                    if (d2*d2 + d3*d3) <= (delta_fit*delta_fit):
+                        im_centers[0].append((x + d2))
+                        im_centers[1].append((y + d3))
+                        im_centers[2].append(im_dif[(x + d2) % xmax, (y + d3) % ymax])
+            im_centers_ = np.array(im_centers)
+            im_centers_[-1] -= np.min(im_centers_[-1], axis=0)
+            xc = np.sum(im_centers_[0]*im_centers_[-1], axis=0) / np.sum(im_centers_[-1], axis=0)
+            yc = np.sum(im_centers_[1]*im_centers_[-1], axis=0) / np.sum(im_centers_[-1], axis=0)
+            Xh = np.array([xc, yc, h]).T
+
+        return Xh
+
+    def _fftalign_2d(self, im1, im2, center=[0, 0]):
+        """
+        Inputs: 2 2D images <im1>, <im2>, the expected displacement <center>, the maximum displacement <max_disp> around the expected vector.
+        This computes the cross-cor between im1 and im2 using fftconvolve (fast) and determines the maximum
+        """
+        from scipy.signal import fftconvolve
+        im2_ = np.array(im2[::-1, ::-1], dtype=float)
+        im2_ -= np.mean(im2_)
+        im2_ /= np.std(im2_)
+        im1_ = np.array(im1, dtype=float)
+        im1_ -= np.mean(im1_)
+        im1_ /= np.std(im1_)
+        im_cor = fftconvolve(im1_, im2_, mode='full')
+
+        sx_cor, sy_cor = im_cor.shape
+        center_ = np.array(center) + np.array([sx_cor, sy_cor]) / 2.
+
+        max_disp = self.parameters['max_disp']
+        x_min = int(min(max(center_[0] - max_disp, 0), sx_cor))
+        x_max = int(min(max(center_[0] + max_disp, 0), sx_cor))
+        y_min = int(min(max(center_[1] - max_disp, 0), sy_cor))
+        y_max = int(min(max(center_[1] + max_disp, 0), sy_cor))
+
+        im_cor0 = np.zeros_like(im_cor)
+        im_cor0[x_min:x_max, y_min:y_max] = 1
+        im_cor = im_cor * im_cor0
+
+        y, x = np.unravel_index(np.argmax(im_cor), im_cor.shape)
+        if np.sum(im_cor > 0) > 0:
+            im_cor[im_cor == 0] = np.min(im_cor[im_cor > 0])
+
+        return (-(np.array(im_cor.shape) - 1) / 2. + [y, x]).astype(int)
+
+    def _run_analysis(self, fragmentIndex: int):
+        fixedImage = self._filter(
+            self.dataSet.get_fiducial_image(0, fragmentIndex))
+        offsets = []
+        im2 = fixedImage.copy()
+        for channel in self.dataSet.get_data_organization().get_data_channels():
+            im_beads = self._filter(self.dataSet.get_fiducial_image(channel, fragmentIndex))
+            im1 = im_beads.copy()
+            Txyzs = []
+            dic_ims1 = self._get_tiles(im1)
+            dic_ims2 = self._get_tiles(im2)
+            for key in dic_ims1:
+                im1_ = dic_ims1[key][0]
+                im2_ = dic_ims2[key][0]
+
+                Xh1 = self._get_local_max(im1_, np.mean(im1_) + np.std(im1_) * self.parameters['threshold_sigma'])
+                Xh2 = self._get_local_max(im2_, np.mean(im2_) + np.std(im2_) * self.parameters['threshold_sigma'])
+
+                tx, ty = self._fftalign_2d(im1_, im2_, center=[0, 0])
+                nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(Xh1[:, :2])
+                distances, indices = nbrs.kneighbors(Xh2[:, :2] + [tx, ty])
+                keep = distances.flatten() < 3
+                indices_ = indices.flatten()[keep]
+                if len(indices_) > 0:
+                    try:
+                        Txyz = np.median(Xh2[keep, :2] - Xh1[indices_, :2], 0)
+                    except Exception:
+                        print("Xh1", Xh1)
+                        print("Xh2", Xh2)
+                        print("keep", keep)
+                        print("indices_", indices_)
+                        raise
+                    Txyzs.append(Txyz)
+                else:
+                    print("No kept beads, fragmentIndex", fragmentIndex, ", channel", channel, ", tile", key)
+            offsets.append(np.median(Txyzs, 0))
         transformations = [transform.SimilarityTransform(
             translation=[-x[1], -x[0]]) for x in offsets]
         self._process_transformations(transformations, fragmentIndex)
